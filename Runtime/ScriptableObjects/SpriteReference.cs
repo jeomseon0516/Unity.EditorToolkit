7041bd692ab99e12757bae8162333ae86895ffce
jeomseon0516 <126733377+jeomseon0516@users.noreply.github.com>
2026-08-12 12:51:35 +0900

    chore: sync in-progress package changes

--- Runtime/ScriptableObjects/SpriteReference.cs
+++ Runtime/ScriptableObjects/SpriteReference.cs
@@ -1,11 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace Jeomseon.ScriptableObjects
 {
     [CreateAssetMenu(fileName = "SpriteReference", menuName = "Scriptable Object/Sprite Reference")]
-    public sealed class SpriteReference : UnityEngine.ScriptableObject
+    public sealed class SpriteReference : ScriptableObject
     {
         [field: SerializeField] public Sprite Sprite { get; private set; }
     }
